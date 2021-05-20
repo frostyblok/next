e48dd57bf0716d6a513657aa1c1307fe0e077ab7
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-05-20 15:54:42 +0100

    Setup base for frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import './notification'
+import "./custom_bootstrap.js"
+import '../stylesheets/application'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
