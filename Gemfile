154844f5c80ccd331533349a6c76cf8cddcdacb7
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-05-25 10:09:06 +0100

    Update Specs

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~> 3.5'
+  gem 'rspec-rails', '~> 4.0'
   gem 'database_cleaner'
   gem 'pry-rails'
   gem 'shoulda-matchers'
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :body }
+  it { should have_one_attached(:image) }
+  it { should define_enum_for(:state).with(draft: 0, published: 1) }
 end
